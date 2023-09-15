4766d4533d40b28f9e98193650cbb401e70430d0
Marcos <fragoso.ufcg@gmail.com>
2023-09-15 10:00:22 -0300

    javadoc usuario repository

--- src/main/java/com/bank/repositories/UsuarioRepository.java
+++ src/main/java/com/bank/repositories/UsuarioRepository.java
@@ -2,9 +2,12 @@ package com.bank.repositories;
 
 import com.bank.domain.usuario.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
+/**
+ * Interface que estende JpaRepository (que já tem métodos pré definidos para criar, ler, alterar e deletar em uma entidade)
+ * Utilizada para acessar os dados de Usuario através da chave primária do tipo Long.
+ */
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     /**
      * Método que busca um determinado usuário pelo cpf.
